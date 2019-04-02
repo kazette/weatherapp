8c44e3a67cb1235d0a5060bcfc1ca4454e9681f9
kazette <47123200+kazette@users.noreply.github.com>
2019-04-02 09:56:19 +0200

    g
    g

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+	render() {
+		return (
+<div>
+<h1 className="title-container__title">What's the Weather Today?</h1>
+<h3 className="title-container__subtitle">Find out the temperature, conditions and more</h3>
+</div>
+			);
+	}
+};
+
+export default Titles;
